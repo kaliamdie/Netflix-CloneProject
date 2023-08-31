09ad73d4b6598545ca4de5f2a43dc3ad442deee3
kaliamdie <girmakalkidan77@gmail.com>
2023-08-31 14:05:02 -0600

    add style

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,92 +1,92 @@
-const express = require('express');
-const router=express.Router()
-const bcrypt=require("bcryptjs")
-const userdb = require('../models/userSchema');
-const authenticate=require("../middleware/authentication")
-router.post("/signup",async(req,res)=>{
-  const {fname,lname,password,email}=req.body
-  if(!fname|| !lname|| !password|| !email){
-    res.status(422).json({error:"fill all the details"})
-  }
-  try{
-const preuser= await userdb.findOne({email:email})
-if(preuser){
-    res.status(422).json({error:"email already exist"})
-}else{
-  const finalUser = new userdb({
-    fname,
-    lname,
-    email,
-    password
-});
+// const express = require('express');
+// const router=express.Router()
+// const bcrypt=require("bcryptjs")
+// const userdb = require('../models/userSchema');
+// const authenticate=require("../middleware/authentication")
+// router.post("/signup",async(req,res)=>{
+//   const {fname,lname,password,email}=req.body
+//   if(!fname|| !lname|| !password|| !email){
+//     res.status(422).json({error:"fill all the details"})
+//   }
+//   try{
+// const preuser= await userdb.findOne({email:email})
+// if(preuser){
+//     res.status(422).json({error:"email already exist"})
+// }else{
+//   const finalUser = new userdb({
+//     fname,
+//     lname,
+//     email,
+//     password
+// });
 
 
-    const storeData=await finalUser.save()
-    res.status(201).json({status:201,storeData});
-    // console.log(storeData)
+//     const storeData=await finalUser.save()
+//     res.status(201).json({status:201,storeData});
+//     // console.log(storeData)
 
-}
-  } catch (error) {
-    console.error(error); // Log the actual error for debugging purposes
-    res.status(500).json({ error: "Server error" }); // Send a generic error response to the client
-  }
+// }
+//   } catch (error) {
+//     console.error(error); // Log the actual error for debugging purposes
+//     res.status(500).json({ error: "Server error" }); // Send a generic error response to the client
+//   }
   
-})
-  //user login
-  router.post("/login", async (req, res) => {
-    const { password, email } = req.body;
+// })
+//   //user login
+//   router.post("/login", async (req, res) => {
+//     const { password, email } = req.body;
   
-    if (!password || !email) {
-      return res.status(422).json({ error: "Fill in all the details" });
-    }
+//     if (!password || !email) {
+//       return res.status(422).json({ error: "Fill in all the details" });
+//     }
   
-    try {
-      const userValid = await userdb.findOne({ email: email });
+//     try {
+//       const userValid = await userdb.findOne({ email: email });
   
   
-      if (!userValid) {
-        return res.status(422).json({ error: "User not found" });
-      }
+//       if (!userValid) {
+//         return res.status(422).json({ error: "User not found" });
+//       }
     
-      const isMatch = await bcrypt.compare(password, userValid.password);
+//       const isMatch = await bcrypt.compare(password, userValid.password);
     
-      if (!isMatch) {
-        return res.status(422).json({ error: "Incorrect password" });
+//       if (!isMatch) {
+//         return res.status(422).json({ error: "Incorrect password" });
       
-      }else{
-        const token = await userValid.generateAuthtoken();
-        res.cookie("usercookie", token, {
-          expires: new Date(Date.now() + 900000000),
-          httpOnly: true
-        }); 
-        const result={
-          userValid,
-          token
-        }
-        res.status(201).json({status:201,result})
+//       }else{
+//         const token = await userValid.generateAuthtoken();
+//         res.cookie("usercookie", token, {
+//           expires: new Date(Date.now() + 900000000),
+//           httpOnly: true
+//         }); 
+//         const result={
+//           userValid,
+//           token
+//         }
+//         res.status(201).json({status:201,result})
      
-        // console.log(token);
-      }
+//         // console.log(token);
+//       }
   
-      // Token generation
+//       // Token generation
    
      
     
   
-      // res.json({ token }); // Send the token in the response
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Server error" });
-    }
-  });
-  //user vaild
-  router.get("/validuser",authenticate,async(req,res)=>{
-try {
-  const ValidUserOne=await userdb.findOne({_id:req.userId})
-  res.status(201).json({ValidUserOne }); 
-} catch (error) {
-  res.status(401).json({ error: "Server error" }); 
-}
-  })
+//       // res.json({ token }); // Send the token in the response
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: "Server error" });
+//     }
+//   });
+//   //user vaild
+//   router.get("/validuser",authenticate,async(req,res)=>{
+// try {
+//   const ValidUserOne=await userdb.findOne({_id:req.userId})
+//   res.status(201).json({ValidUserOne }); 
+// } catch (error) {
+//   res.status(401).json({ error: "Server error" }); 
+// }
+//   })
   
-module.exports=router
+// module.exports=router
--- client/src/components/Poster.js
+++ client/src/components/Poster.js
@@ -3,14 +3,14 @@ import axios from '../utils/axios';
 import { requests } from '../utils/api';
 
 export default function Poster() {
-  const [movie, setMovie] = useState(null); // Change initial state to null or an empty object
+  const [movie, setMovie] = useState(null);
 
   async function fetchData() {
     const request = await axios.get(requests.fetchNetflixOriginals);
     const randomMovieIndex = Math.floor(Math.random() * request.data.results.length);
     const randomMovie = request.data.results[randomMovieIndex];
-    console.log(randomMovie); // Check if the fetched random movie object is valid
-    setMovie(randomMovie); // Set the random movie object in the state
+    console.log(randomMovie);
+    setMovie(randomMovie);
     return request;
   }
 
@@ -18,22 +18,33 @@ export default function Poster() {
     fetchData();
   }, []);
 
-  console.log(movie);
-
   return (
     <header
-      className=""
+      className="text-white relative"
       style={{
         backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`,
         backgroundPosition: 'center center',
+        height: '448px',
       }}
     >
-      <div className='banner'>
-        <h1> {movie?.name || movie?.title || movie?.original_name}</h1>
-      </div>
-      <div className='buttons'>
-        <button>Play</button>
-        <button>My List</button>
+      <div className="ml-30 pt-36 absolute bottom-0 left-0">
+        <h1 className="text-5xl font-bold pb-1">
+          {movie?.name || movie?.title || movie?.original_name}
+        </h1>
+        <div className="text-black space-x-4">
+          <button className="bg-red-600 text-white px-4 py-2 rounded">
+            Play
+          </button>
+          <button className="bg-gray-800 text-white px-4 py-2 rounded">
+            My List
+          </button>
+        </div>
+        <p
+          className="mt-4 text-sm text-gray-300 max-w-sm"
+          style={{ lineHeight: '1.3' }}
+        >
+          {movie?.overview}
+        </p>
       </div>
     </header>
   );
