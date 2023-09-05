d8f808687fd49569d3b474e7e20da4c4990394e8
kaliamdie <girmakalkidan77@gmail.com>
2023-09-05 15:43:23 -0600

    fixed some issues

--- backend/config.js
+++ backend/config.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
-const mongoConfig = async () => {
-  try {
-    const result = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('Database connected: ', result.connection.host);
-  } catch (err) {
-    console.log('Mongo error: ', err);
-  }
+const connectDB = async () => {
+    try {
+        const result = await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Database connected: ', result.connection.host);
+    } catch (err) {
+        console.error('Mongo error: ', err);
+    }
 };
 
-module.exports = mongoConfig;
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -2,30 +2,27 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const cookieParser=require("cookie-parser")
+const cookieParser = require("cookie-parser");
 const cors = require('cors');
 const PORT = 8000;
-const connectDB = require('./config');
-const router=require("./routes/router")
- 
+const connectDB = require("./config")
+const router = require("./routes/router");
 
-//
+// Middleware
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(cors());
-//
 
+// Connect to the database
+connectDB();
 
-app.use(router)
+// Routes
+app.use(router);
+
 app.get("/", (req, res) => {
     res.json({ message: "hello" });
 });
 
-
-connectDB();
-
 app.listen(PORT, () => {
     console.log('Listening to the port: ' + PORT);
 });
-
-
